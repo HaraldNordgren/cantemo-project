a3d85cc723f9093c1ceb551f792c1fc6ccfc0d97
HaraldNordgren <haraldnordgren@gmail.com>
2016-04-12 21:01:09 +0200

    Metadata can now be changed and displayed next to title

--- mysite/image_bank/urls.py
+++ mysite/image_bank/urls.py
@@ -2,12 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-#app_name = "image_bank"
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^$', views.IndexView.as_view(), name='index'),
-    url('path_search', views.path_search, name='pathsearch'),
-    url(r'name_search', views.name_search, name='namesearch'),
+    #url('path_search', views.path_search, name='pathsearch'),
+    #url(r'name_search', views.name_search, name='namesearch'),
     url('stored-images', views.show_image, name='imageshow')
 ]
--- mysite/image_bank/views.py
+++ mysite/image_bank/views.py
@@ -4,33 +4,8 @@ from django.views import generic
 
 from .models import BankImage
 from .constants import *
-    
-def remove_prefix(string):
-    return "/".join(string.split("/")[3:])
-    #return string.lstrip("/")
 
-def index(request):
-
-    if request.GET.get('namesearch'):
-        search = request.GET.get('namesearch')
-        images = BankImage.objects.filter(path__icontains=search)
-        #searched = True
-    else:
-        search = ""
-        images = BankImage.objects.all()
-        #searched = False
-
-    paths = []
-    for im in images:
-        short_path = im.path
-        full_path = stored_images + im.path
-        paths.append( (short_path, full_path) )
-
-    context = {
-            'images': paths,
-            'search': search}
-    return render(request, 'image_bank/index.html', context)
-
+"""
 def path_search(request):
     return render(request, 'image_bank/path_search.html')
 
@@ -38,18 +13,46 @@ def name_search(request):
     images = BankImage.objects.filter(path__icontains=request.GET.get('q'))
     context = { 'images': images }
     return render(request, 'image_bank/name_search.html', context)
+"""
+    
+def remove_prefix(string):
+    return "/".join(string.split("/")[3:])
+
+def index(request):
+
+    if request.GET.get('namesearch'):
+        search = request.GET.get('namesearch')
+        images = BankImage.objects.filter(path__icontains=search)
+    else:
+        search = ""
+        images = BankImage.objects.all()
+
+    im_data = []
+    for im in images:
+        short_path = im.path
+        full_path = stored_images + im.path
+        im_data.append( (short_path, full_path, im.metadata) )
+
+    context = {
+            'images': im_data,
+            'search': search}
+    return render(request, 'image_bank/index.html', context)
 
 def show_image(request):
 
-    #print(request.path_info)
     short_path = remove_prefix(request.path_info)
-    print("    " + short_path)
-    metadata = BankImage.objects.filter(path=short_path)[0].metadata
     full_path = watched_folder + short_path
 
+    if request.method == 'POST':
+        im = BankImage.objects.get(path=short_path)
+        metadata = request.POST['metadata']
+        im.metadata = metadata
+        im.save()
+    else:
+        metadata = BankImage.objects.get(path=short_path).metadata
+    
     context = {
             'im_path' : full_path,
             'metadata': metadata }
-    #print(remove_prefix(request.path_info))
-    
+
     return render(request, 'image_bank/show_image.html', context)
