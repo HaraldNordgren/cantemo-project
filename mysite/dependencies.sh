4b14d8d29171fd296c2bbe54e7efbf6934bef991
Harald Nordgren <haraldnordgren@gmail.com>
2016-04-13 07:16:12 +0200

    Updated dependencies

--- mysite/dependencies.sh
+++ mysite/dependencies.sh
@@ -1,4 +1,13 @@
+#Dependencies
+sudo -H pip3 install django
 sudo pip3 install watchdog
-#sudo -H pip3 install Celery
-#sudo apt-get install libvpx-dev
-sudo apt-get install ffmpeg
+
+compile/install ffmpeg
+get google chrome
+
+#Preparation
+python3 manage.py migrate
+
+#Run
+python3 manage.py runserver
+
